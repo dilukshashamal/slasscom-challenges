be8e6f44c888d5eeb8c3b825bb4a2446a1cfa96b
hbkdshamal <dilukshashamal2001@gmail.com>
2022-12-22 19:11:24 +0530

    add few changes

--- challenge3.java
+++ challenge3.java
@@ -5,17 +5,55 @@ public class challenge3 {
     public static void main(String[] arg){
         int[] array = {47,84,75,21,14,14,79};
         //sorted array
-        Arrays.sort(array);
+        //Arrays.sort(array);
+
+        //sorting array
+        sorting(array);
+        for(int i : array){
+            System.out.print(i+" ");
+        }
         int l = array.length;
 
         //call classes
         int m1 = median(l,array);
         double m2 = mean(l,array);
         int m3 = mode(l,array);
+        System.out.println("");
         System.out.println("Median :"+m1);
         System.out.println("Mean   :"+m2);
         System.out.println("Mode   :"+m3);
 
+        //find prime numbers
+        System.out.println("");
+        System.out.println("Prime Numbers");
+        for(int i =0; i<l;i++){
+            boolean checkPrime = true;
+            for(int j=2;j<array[i];j++){
+                if(array[i]%j==0){
+                    checkPrime = false;
+                    break;
+                }
+            }
+            if(checkPrime){
+                System.out.print(array[i]+" ");
+            }
+        }
+
+    }
+
+    //sorting
+    public static void sorting(int arr[]){
+        for(int i = 0; i<arr.length-1;i++){
+            int indexN = i;
+            for(int j = i+1;j<arr.length;j++){
+                if(arr[j]<arr[indexN]){
+                    indexN = j;
+                }
+            }
+            int sNumber = arr[indexN];
+            arr[indexN] = arr[i];
+            arr[i] = sNumber;
+        }
     }
 
     //find median
